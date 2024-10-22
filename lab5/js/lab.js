0b507b66f78e521692ea29e01a08762868c6f69a
juliafart <jumigome@ucsc.edu>
2024-10-21 19:05:15 -0700

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -4,8 +4,25 @@
 
 // Define Variables
 make = "Mazada";
-model
+model = "Miada";
+color = "forrest green";
+year = 1997;
+
+// calculate
+age = 2024 - year;
+
+//output
+document.writeln ("Make: "+ make + "<br>");
+document.writeln ("Model: " + model + "<br>");
+document.writeln ("Color: "+ color + "<br>");
+document.writeln ("Year: " + year + "<br>");
+document.writeln ("Age: " + age + "years<br>");
+
 // Constants
+make = "Mazada";
+model = "Miada";
+color = "forrest green";
+year = 1997;
 
 // Functions
 
