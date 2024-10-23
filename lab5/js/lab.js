0621055ccb8b8ed9ae238916aba383b7adfd8edf
juliafart <jumigome@ucsc.edu>
2024-10-22 18:02:32 -0700

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -18,25 +18,3 @@ document.writeln ("Model: " + model + "<br>");
 document.writeln ("Color: "+ color + "<br>");
 document.writeln ("Year: " + year + "<br>");
 document.writeln ("Age: " + age + "years<br>");
-
-// Constants
-make = "Mazada";
-model = "Miada";
-color = "forrest green";
-year = 1997;
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
