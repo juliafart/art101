23b4c6665a21b0e31a510ef305116a5ab2e23858
juliafart <jumigome@ucsc.edu>
2024-10-28 19:45:33 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,3 +1,24 @@
 // index.js - Learning Arrays and Objects
 // Author: Julia Gomez
-// Date: 10/24/24
+// Date: 10/28/24
+
+function myVines () {
+    var vine = window.prompt("hi there! what kind of grapes would you like to grow on the vine today?");
+    console.log("vine=", vine);
+// split string to array
+    var vineArray = vine.split ('');
+    console.log("vineArray =", vineArray);
+// sort the Array
+    var vineArraySort = vineArray.sort ();
+    console.log("vineArraySort = ", vineArraySort);
+//join up array
+    var vineSorted = vineArraySort.join ('');
+    console.log ("vineSorted = ", vineSorted);
+//return
+return vineSorted;
+}
+
+//output
+
+document.writeln ("OH! wow, look at that I just grew a new kind of grape: ",
+    myVines(), "</br>");
