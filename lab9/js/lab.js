c0555bf1e9d0a5789ca8c4b63292ac5bc1e8721d
juliafart <jumigome@ucsc.edu>
2024-11-05 14:21:16 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -12,28 +12,28 @@ $("#button-challenge").click(function() {
 })
 
 //for problem section
-$("#problem").append("<button id='button-problem'>Make Special</button>");
+$("#Problem").append("<button id='button-problem'>Make Special</button>");
 
 // add a click listener to the challenge button
 $("#button-problem").click(function() {
     //now add (or subtract) the "special" class to the section
-    $("#problem").toggleClass("special");
+    $("#Problem").toggleClass("special");
 })
 
 //for relection section
-$("#reflection").append("<button id='button-reflection'>Make Special</button>");
+$("#Reflection").append("<button id='button-reflection'>Make Special</button>");
 
 // add a click listener to the challenge button
 $("#button-reflection").click(function() {
     //now add (or subtract) the "special" class to the section
-    $("#reflection").toggleClass("special");
+    $("#Reflection").toggleClass("special");
 })
 
 //for results section
-$("#results").append("<button id='button-results'>Make Special</button>");
+$("#Results").append("<button id='button-results'>Make Special</button>");
 
 // add a click listener to the challenge button
 $("#button-results").click(function() {
     //now add (or subtract) the "special" class to the section
-    $("#results").toggleClass("special");
+    $("#Results").toggleClass("special");
 })
