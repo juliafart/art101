72721f0aa3c3dc945a62aa495be858221b49e348
juliafart <jumigome@ucsc.edu>
2024-11-07 17:34:20 -0800

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,6 +1,11 @@
-// index.js - 
-// Author: Julia Gomez
-// Date: 11/4/24
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Julia Gomez
+   Date: 11/7/2024
+*/
 
 // add button to challenge section
 $("#Challenge").append("<button id='button-challenge'>Make Special</button>");
