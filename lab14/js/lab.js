8842361bccc5bb2f75f2529d0fc3a3e826e2110e
juliafart <jumigome@ucsc.edu>
2024-11-21 20:52:20 -0800

    why it mad

---
+++ lab14/js/lab.js
@@ -0,0 +1,8 @@
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Julia Gomez
+   Date: 11/7/2024
+*/
